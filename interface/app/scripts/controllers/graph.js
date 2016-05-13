a4c8c1958c7b6eb9228df4e88cdfcb01e9c1367f
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-05-13 04:25:10 -0300

    Adicionar esqueleto do controlador de gráficos

---
+++ interface/app/scripts/controllers/graph.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interfaceApp.controller:GraphCtrl
+ * @description
+ * # GraphCtrl
+ * Controller of the interfaceApp
+ */
+angular.module('interfaceApp')
+  .controller('GraphCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
