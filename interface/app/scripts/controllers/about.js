aa9deb6caed698e8bf605c4daaeb81a356063286
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-05-13 01:25:25 -0300

    Criar projeto com yo generator-angular

---
+++ interface/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interfaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the interfaceApp
+ */
+angular.module('interfaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ interface/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interfaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the interfaceApp
+ */
+angular.module('interfaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
