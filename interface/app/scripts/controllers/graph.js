2ee3efe891b7b5d9c20346e9b487f34043cc5a88
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-05-13 15:14:21 -0300

    Esqueleto do controlador de gráficos

--- interface/app/scripts/controllers/graph.js
+++ interface/app/scripts/controllers/graph.js
@@ -9,9 +9,5 @@
  */
 angular.module('interfaceApp')
   .controller('GraphCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
