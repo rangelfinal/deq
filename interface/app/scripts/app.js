2bac2dbae31b955861807db71b15ae7231182070
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-05-13 01:45:38 -0300

    Identação

--- interface/app/scripts/app.js
+++ interface/app/scripts/app.js
@@ -20,18 +20,18 @@ angular
     'ngTouch'
   ])
   .config(function ($routeProvider) {
-    $routeProvider
+    	$routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        	templateUrl: 'views/main.html',
+        	controller: 'MainCtrl',
+        	controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        	templateUrl: 'views/about.html',
+        	controller: 'AboutCtrl',
+        	controllerAs: 'about'
       })
       .otherwise({
-        redirectTo: '/'
+        	redirectTo: '/'
       });
   });
