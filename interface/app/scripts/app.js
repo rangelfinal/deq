20b7882a6f4ce1bd2b3b27f9eea3d43948754b9d
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-05-29 17:58:31 -0300

    Corrigir whitespace

--- interface/app/scripts/app.js
+++ interface/app/scripts/app.js
@@ -21,18 +21,18 @@ angular
     'chart.js'
   ])
   .config(function ($routeProvider) {
-    	$routeProvider
+    $routeProvider
       .when('/', {
-        	templateUrl: 'views/main.html',
-        	controller: 'MainCtrl',
-        	controllerAs: 'main'
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       })
       .when('/about', {
-        	templateUrl: 'views/about.html',
-        	controller: 'AboutCtrl',
-        	controllerAs: 'about'
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
       })
       .otherwise({
-        	redirectTo: '/'
+        redirectTo: '/'
       });
   });
