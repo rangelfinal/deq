def81a5113470149b2f953d16912fd6af0c131ec
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-08-01 02:26:00 -0300

    Adicionar mensagem de conclusão de instalação

--- Setup/setup.py
+++ Setup/setup.py
@@ -11,3 +11,4 @@ if __name__ == '__main__':
     install('pyserial')
     if not os.path.isfile("../Dessanilizador/DEQ.sqlite"):
         createDB.createDB
+    win32api.MessageBox(0, 'Instalação concluida com sucesso', 'Dessanilizador', 0x00001000) 
