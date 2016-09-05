cd605463fca445f4be75498d3be6828ce5c3921f
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-09-05 12:04:07 -0300

    Adicionar *.zip ao .gitignore

--- setup.bat
+++ setup.bat
@@ -1,4 +1,11 @@
 @echo off
+echo Instalando Git
+Setup/Git-2.10.0-32-bit.exe
+echo Fazendo download do projeto
+git clone https://projetodeq:senhadoprojetdeq@github.com/rangelfinal/deq.git
+cd Setup
+echo Instalando Python
+python-3.5.2.exe
 echo Instalando dependencias do projeto.
 pip install -r requirements.txt >nul
 echo Criando banco de dados
